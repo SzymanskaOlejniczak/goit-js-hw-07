e28d74d70c032a1eceea2ccf493654e1485a06b0
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-09-27 19:42:29 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryPictures = document.querySelector('.gallery');
+
+// console.log(galleryPictures);
+
+const newGalery = galleryItems
+    .map(({preview, original, description}) => 
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" alt="${description}" />
+        </a>`)
+        .join('');
+
+galleryPictures.insertAdjacentHTML("afterbegin",newGalery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    canimationSpeed: 250,
+    captionPosition: 'bottom',
+});
+galleryPictures.addEventListener("click",lightbox);
